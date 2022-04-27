30a8189dc848cb23be28fc87f0b5b00317d96738
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2022-04-26 23:42:45 -0600

    [new] Method that validates the entered url.
    Creation of the method that checks that invalid urls cannot enter the system. Using regular expression to check urls.

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,7 +1,10 @@
 class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-
+  
+  #Regular expression to validate the full URL.
+  URL_REGEX = Regexp.new('\A(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?\Z')
+  validates :full_url, presence: true
   validate :validate_full_url
 
   def short_code
@@ -13,6 +16,10 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    #The =~ operator matches the regular expression against a string.
+    if not full_url =~ URL_REGEX
+      errors.add(:full_url, 'The digited URL is not valid, please enter a correct structure of URL.')
+    end
   end
 
 end
