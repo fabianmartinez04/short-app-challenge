1753405647534a685bb8d870cacf7c48f6fd1d68
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2022-04-27 19:51:44 -0600

    [new] Creating the index method.
    The index method was created, which returns the 100 most accessed records.
    Important, the method has not been fully tested.

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,9 +1,13 @@
+require 'json'
 class ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
+  #Return the top 100 most frequently accessed shortcodes.
   def index
+    @short_urls = ShortUrl.order(click_count: :desc).limit(100)
+    render json: { :urls => @short_urls}, status: :ok
   end
 
   def create
