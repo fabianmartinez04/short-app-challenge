532acef68f4ac67d37aa4b9ea83e06049678ff9a
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2022-04-28 14:45:25 -0600

    [fix] Added comments to created methods.

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,12 +4,15 @@ class ShortUrlsController < ApplicationController
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
-  #Return the top 100 most frequently accessed shortcodes.
+  # Return the top 100 most frequently accessed shortcodes.
   def index
     @urls = ShortUrl.order(click_count: :desc).limit(100)
     render json: {:urls => @urls}, status: :ok
   end
 
+  # Register the full url within the database.
+  #
+  # Returns a json containing the information about the ful_url that got created.
   def create
     full_url = params[:full_url]
     @short_url = ShortUrl.create(full_url: full_url)
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,24 +2,24 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
   
-  #Regular expression to validate the full URL.
+  # Regular expression to validate the full URL.
   URL_REGEX = Regexp.new('\A(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?\Z')
   validates :full_url, presence: true
   validate :validate_full_url
 
-  #Gets the id from db as a parameter and calls the function encode_id(id).
-  #Returns the short code in base62.
+  # Gets the id from db as a parameter and calls the function encode_id(id).
+  # Returns the short code in base62.
   def short_code
     return nil if id == nil
     encode_id(id)
   end
 
-  #This algorithm encodes the id in base10 and returns a string in base62.
+  # This algorithm encodes the id in base10 and returns a string in base62.
   def encode_id(id)
     code = ""
     length = CHARACTERS.length
 
-    #id needs to be > 0
+    # id needs to be > 0
     while id.positive?
       code = CHARACTERS[id % length] + code
       id = id / length
@@ -34,7 +34,7 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
-    #The =~ operator matches the regular expression against a string.
+    # The =~ operator matches the regular expression against a string.
     if not full_url =~ URL_REGEX
       errors.add(:full_url, 'The digited URL is not valid, please enter a correct structure of URL.')
     end
